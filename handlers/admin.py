8f542ae371ac4a4fa21d99ff8fcaa7709fde96ac
Iskandarov9799 <iskandarov.qudrat98@gmail.com>
2026-03-21 10:44:58 +0500

    admin.py
    Signed-off-by: Iskandarov9799 <iskandarov.qudrat98@gmail.com>

--- handlers/admin.py
+++ handlers/admin.py
@@ -6,7 +6,9 @@ from aiogram.fsm.context import FSMContext
 
 from database.db import (
     get_all_users, get_full_stats, add_question,
-    count_questions, delete_all_questions, delete_questions_by_filter
+    count_questions, delete_all_questions, delete_questions_by_filter,
+    get_user_tariff, admin_grant_subscription, admin_revoke_subscription,
+    grant_attestation
 )
 from keyboards.keyboards import (
     admin_keyboard, cancel_keyboard, main_menu_keyboard,
